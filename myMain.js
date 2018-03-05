7d1704b659079c6394303fec56fae717509da301
15abarke <36679352+15abarke@users.noreply.github.com>
2018-03-05 18:33:51 -0500

    Update myMain.js

--- myMain.js
+++ myMain.js
@@ -28,7 +28,7 @@ var myFunctionHolder ={};
 
     var mapObject = L.map('mapDivId').setView([3.090739, 17.404454], 4);
     var baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiMTIzODIiLCJhIjoiY2pjaTRscWllMmV0ZTMzbnptZXppczA1MCJ9.DbUREsZcMMNHEBwqytnWnA', {
-      maxZoom: 100,
+      maxZoom: 18,
       attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
     }).addTo(mapObject);
     var cfg = {
